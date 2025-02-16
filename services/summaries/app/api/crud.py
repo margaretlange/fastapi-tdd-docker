c69c0e8caa2caac440881d3cd498390fc240f26f
margaretlange <margaret.meek@gmail.com>
2025-02-16 12:37:09 -0800

    reverting crud

--- services/summaries/app/api/crud.py
+++ services/summaries/app/api/crud.py
@@ -7,30 +7,30 @@ from app.models.tortoise import TextSummary
 
 
 # summary crud
-async def post_summary(payload: SummaryPayloadSchema) -> int:
+async def post(payload: SummaryPayloadSchema) -> int:
     summary = TextSummary(url=payload.url, summary="")
     await summary.save()
     return summary.id
 
 
-async def get_summary(id: int) -> Union[dict, None]:
+async def get(id: int) -> Union[dict, None]:
     summary = await TextSummary.filter(id=id).first().values()
     if summary:
         return summary
     return None
 
 
-async def get_all_summaries() -> List:
+async def get_all() -> List:
     summaries = await TextSummary.all().values()
     return summaries
 
 
-async def delete_summary(id: int) -> int:
+async def delete(id: int) -> int:
     summary = await TextSummary.filter(id=id).first().delete()
     return summary
 
 
-async def put_summary(id: int, payload: SummaryPayloadSchema) -> Union[dict, None]:
+async def put(id: int, payload: SummaryPayloadSchema) -> Union[dict, None]:
     summary = await TextSummary.filter(id=id).update(
         url=payload.url, summary=payload.summary
     )
