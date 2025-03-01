d1a0e13f1ce04301e928469b0223fc4a9579409f
margaretlange <margaret.meek@gmail.com>
2025-03-01 15:32:22 -0800

    test out

--- request_auth0.sh
+++ request_auth0.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-curl --request POST \
+response=$(curl --request POST \
   --url https://dev-lhn1ufqgkgz8lk5w.us.auth0.com/oauth/token \
   --header 'content-type: application/json' \
-  --data '{"client_id":"u86kVPjj9Y5RxoSXgtJDvAXFOKTe5DPG","client_secret":"yApiTedCGQ_LoKLgLFxplJMlRAPpcTbPni71cIsBjZSJBZ7gcTOs1XNaLqlhsM0m","audience":"https://hello-world.example.com","grant_type":"client_credentials"}'
+  --data '{"client_id":"u86kVPjj9Y5RxoSXgtJDvAXFOKTe5DPG","client_secret":"yApiTedCGQ_LoKLgLFxplJMlRAPpcTbPni71cIsBjZSJBZ7gcTOs1XNaLqlhsM0m","audience":"https://hello-world.example.com","grant_type":"client_credentials"}')
+echo $response | jq -r .access_token
