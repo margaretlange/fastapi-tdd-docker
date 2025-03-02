3900f48eb3846ab57787b044fc771cf699135ba9
margaretlange <margaret.meek@gmail.com>
2025-03-01 18:00:14 -0800

    giving up on pulling token

--- test_curl_two.sh
+++ test_curl_two.sh
@@ -3,5 +3,5 @@ AUTH0_CLIENT_ID=u86kVPjj9Y5RxoSXgtJDvAXFOKTe5DPG
 AUTH0_CLIENT_SECRET=yApiTedCGQ_LoKLgLFxplJMlRAPpcTbPni71cIsBjZSJBZ7gcTOs1XNaLqlhsM0m
 AUTH0_AUDIENCE=https://hello-world.example.com
 
-token=`./request_auth0_token.sh $AUTH0_DOMAIN $AUTH0_CLIENT_ID $AUTH0_CLIENT_SECRET $AUTH0_AUDIENCE`
-echo "AUTH0_ACCESS_TOKEN=$token" >> /home/maggie/.bashrc 
+token=`/home/maggie/fastapi-tdd-docker/.github/request_auth0_token.sh $AUTH0_DOMAIN $AUTH0_CLIENT_ID $AUTH0_CLIENT_SECRET $AUTH0_AUDIENCE`
+echo "AUTH0_ACCESS_TOKEN=$token" 
