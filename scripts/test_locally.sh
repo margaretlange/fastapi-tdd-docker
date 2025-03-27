a4fadc03f777400d2bbf5398176a4ae473811492
margaretlange <margaret.meek@gmail.com>
2025-03-26 22:52:05 -0700

    script to imitate github actions test

---
+++ scripts/test_locally.sh
@@ -0,0 +1,17 @@
+docker build \
+       --tag testingapi:latest \
+       --file ./services/summaries/Dockerfile.prod \
+       "./services/summaries"
+
+docker run -d \
+--name fastapi-tdd \
+-e PORT=8765 \
+-e ENVIRONMENT=dev \
+-e DATABASE_URL=sqlite://sqlite.db \
+-e DATABASE_TEST_URL=sqlite://sqlite.db \
+-e JWT_TEST_ENCODE_KEY=$JWT_TEST_ENCODE_KEY \
+-p 5003:8765 \
+testingapi:latest
+
+docker exec fastapi-tdd python -m pytest .
+
